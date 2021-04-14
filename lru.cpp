01b2c219d13a3151dcc6a08aa3c52980ec1b76cc
evlzhang <zyh991001@gmail.com>
2021-04-14 19:27:13 -0400

    lru foundations

--- lru.cpp
+++ lru.cpp
@@ -1,8 +1,11 @@
 #include "fifo.h"
 #include "page.h"
 #include <queue>
+#include <map>
+
 using namespace std;
 using namespace P;
 void lru(int k,int references[]){
-    
+    //use map with least recently used field in Pages
+    //or use linked list with order by frequency
 }
--- page.h
+++ page.h
@@ -5,7 +5,7 @@ namespace P{
     class Pages{
         int frame_num;
         int reference;
-    
+        
     public:
         Pages(int num,int ref);
         Pages(int ref);
